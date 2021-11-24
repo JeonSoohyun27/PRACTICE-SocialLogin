1db570d021296366dbc86ca204bf67ae594cb1f0
JeonSoohyun <jeonsoohyun27@gmail.com>
2021-11-24 16:57:23 +0900

    숨길항목 지정 및 이동

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from my_settings import  DATABASES, SECRET_KEY
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0i+3uic5$358!v7)f1st+rw+qeb8s+1s@v0aemu3&n*o8pq2vf'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,16 +85,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'socialdb',
-        'USER': 'root',
-        'PASSWORD': 'wjstn2298!',
-        'HOST': 'localhost',
-        'PORT': 3306,
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-0i+3uic5$358!v7)f1st+rw+qeb8s+1s@v0aemu3&n*o8pq2vf'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialdb',
+        'USER': 'root',
+        'PASSWORD': 'wjstn2298!',
+        'HOST': 'localhost',
+        'PORT': 3306,
+    }
+}
